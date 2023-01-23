4b723c5121bafd62e25cb0164c87c0c17a3979f5
Lokesh Nanjundappa <lokesh.nanjundappa@itorizon.com>
2023-01-23 21:30:40 +0530

    added all files

---
+++ JavaScript/stringMetohds.js
@@ -0,0 +1,9 @@
+// string lenght
+
+let ename="lokesh";
+console.log(ename.length);
+
+console.log(ename.slice(3));
+console.log(ename.slice(2, 6));
+console.log(ename.slice(-3));
+console.log(ename.slice(-5,-3));
